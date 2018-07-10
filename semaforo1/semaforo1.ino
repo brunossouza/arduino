b5201eb7d64a76d60cba06da56c26edffea8e68b
Bruno Silva <brunos.souza2@gmail.com>
2018-07-10 18:17:17 -0300

    first commit

---
+++ semaforo1/semaforo1.ino
@@ -0,0 +1,43 @@
+#define RED 10
+#define YELLOW 11
+#define GREEN 12
+#define TMPESPERAR 1
+
+//https://www.arduinoecia.com.br/2013/07/display-7-segmentos.html
+
+void setup(){
+  pinMode(RED, OUTPUT);
+  pinMode(YELLOW, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop(){  
+  onLed("red");
+  onLed("green");
+  onLed("yellow");
+}
+
+void onLed(String cor){
+  Serial.print(cor + "\n");
+  digitalWrite(RED, LOW);
+  digitalWrite(YELLOW, LOW);
+  digitalWrite(GREEN, LOW);
+
+  if(cor == "red"){
+    digitalWrite(RED, HIGH);
+  }
+  if(cor == "green"){
+    digitalWrite(GREEN, HIGH);
+  }
+  if(cor == "yellow"){
+    digitalWrite(YELLOW, HIGH);
+  }
+  contRegr(TMPESPERAR);
+}
+
+void contRegr(int tempo){
+  for(int i = tempo ; i>0 ;i--){
+    delay(1000);
+  }
+}
